df98f5129fd09166da7fa99b05f9c47fe92ebebc
Gabi Mena <gabriela.grojas@gmail.com>
2017-11-29 09:48:19 +0100

    adding exercise

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 'use strict';
 
-var usuario = prompt('Cual es tu nombre?');
-var funcionalidad = prompt('Qué necesitas?');
-var razon = prompt('Cual es el motivo?');
-
-// var counter = {
-//   user: usuario,
-//   funcionality: funcionalidad,
-//   reason: razon
-// };
+// var usuario = prompt('Cual es tu nombre?');
+// var funcionalidad = prompt('Qué necesitas?');
+// var razon = prompt('Cual es el motivo?');
 //
-//  counter.printHistory() {
-  alert("Yo como usario: " + usuario + "necesito " + funcionalidad + "con la finalidad de " + razon);
- // }
- //
- // printHistory();
+// var counter = {
+//    user: usuario = prompt('Cual es tu nombre?'),
+//    funcionality: funcionalidad,
+//    reason: razon
+//  };
+//
+//   counter.printHistory() {
+//   alert("Yo como usario: " + usuario + "necesito " + funcionalidad + "con la finalidad de " + razon);
+//   }
+//
+//   printHistory();
+
+
+ var counter = {
+    user: usuario = prompt('Cual es tu nombre?'),
+    };
+
+   counter.printHistory() {
+   alert("Yo como usario: " + usuario + );
+   }
+
+   printHistory();
