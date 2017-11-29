d212f88ff81a6a4038b1b7759d61f29d6d9e137f
Cris <crisgalasanchez@gmail.com>
2017-11-29 09:22:40 +0100

    archivo inicio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var usuario= prompt(' cual es tu nombre?');
+var funcionalidad = prompt(' que necesitas?');
+var usuario= prompt(' cual es el motivo?');
+
+var counter ={
+  usuario:
+  funcionalidad
+  razon:
+}
